735de705260134f3cc7807730e34ed846980ddfc
ngaionice <julian.myne@gmail.com>
2022-05-11 23:40:47 -0400

    Update data entity definitions

--- app/src/main/java/me/ionice/snapshot/database/Day.kt
+++ app/src/main/java/me/ionice/snapshot/database/Day.kt
@@ -1,16 +1,28 @@
 package me.ionice.snapshot.database
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
+import java.time.LocalDate
 import java.util.*
 
 @Entity(tableName = "day_entry")
-class Day {
+data class Day(
 
     @PrimaryKey(autoGenerate = true)
-    var id: Long = 0L
+    var id: Long = 0L,
 
-    val date: Date = Calendar.getInstance().time
-    var summary: String = ""
+    val date: LocalDate = LocalDate.now(),
+    var summary: String = "",
     var location: String? = null
-}
+)
+
+data class DayWithMetricEntries(
+    @Embedded val day: Day,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "day_id"
+    )
+    val metrics: List<MetricEntry>
+)
--- app/src/main/java/me/ionice/snapshot/database/Metric.kt
+++ app/src/main/java/me/ionice/snapshot/database/Metric.kt
@@ -1,25 +1,23 @@
 package me.ionice.snapshot.database
 
 import androidx.room.*
-import java.util.*
 
 @Entity(tableName = "metric_key")
-class Metric {
-
+data class Metric(
     @PrimaryKey(autoGenerate = true)
-    var id: Long = 0L
+    var id: Long = 0L,
 
-    @ColumnInfo(name = "name")
     var name: String = ""
-}
+)
 
-@Entity(tableName = "metric_entry", primaryKeys = ["metric_id", "date"])
-class MetricEntry(@ColumnInfo(name = "metric_id") val metricId: Long) {
+@Entity(tableName = "metric_entry", primaryKeys = ["metric_id", "day_id"])
+data class MetricEntry(
+    @ColumnInfo(name = "metric_id")
+    val metricId: Long,
 
-    @ColumnInfo(name = "date")
-    val date: Date = Calendar.getInstance().time
-
-}
+    @ColumnInfo(name = "day_id")
+    val dayId: Long
+)
 
 data class MetricWithEntries(
     @Embedded val metric: Metric,
