24a1f9b9378ccdf8d3571b4993d394a96aed10c4
ngaionice <julian.myne@gmail.com>
2022-05-21 23:06:48 -0400

    Add more methods to MetricRepository

--- app/src/main/java/me/ionice/snapshot/data/metric/MetricRepository.kt
+++ app/src/main/java/me/ionice/snapshot/data/metric/MetricRepository.kt
@@ -4,7 +4,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface MetricRepository {
 
-    fun observeKeys(): Flow<List<MetricKey>>
-
     suspend fun insertKey(name: String)
+
+    suspend fun getMetric(key: MetricKey) : Metric?
+
+    suspend fun upsertEntry(entry: MetricEntry)
+
+    suspend fun deleteEntry(entry: MetricEntry)
+
+    fun observeKeys(): Flow<List<MetricKey>>
 }
--- app/src/main/java/me/ionice/snapshot/data/metric/MetricRepositoryImpl.kt
+++ app/src/main/java/me/ionice/snapshot/data/metric/MetricRepositoryImpl.kt
@@ -7,14 +7,31 @@ import me.ionice.snapshot.data.SnapshotDatabase
 
 class MetricRepositoryImpl(private val database: SnapshotDatabase) : MetricRepository {
 
-    override fun observeKeys(): Flow<List<MetricKey>> {
-        return database.metricDao.getAllKeys()
-    }
-
     override suspend fun insertKey(name: String) {
         withContext(Dispatchers.IO) {
             database.metricDao.insertKey(MetricKey(name = name))
         }
     }
 
+    override suspend fun getMetric(key: MetricKey): Metric? {
+        return withContext(Dispatchers.IO) {
+            database.metricDao.getMetric(key.id)
+        }
+    }
+
+    override suspend fun upsertEntry(entry: MetricEntry) {
+        withContext(Dispatchers.IO) {
+            database.metricDao.upsertEntry(entry)
+        }
+    }
+
+    override suspend fun deleteEntry(entry: MetricEntry) {
+        withContext(Dispatchers.IO) {
+            database.metricDao.deleteEntry(entry)
+        }
+    }
+
+    override fun observeKeys(): Flow<List<MetricKey>> {
+        return database.metricDao.getAllKeys()
+    }
 }
