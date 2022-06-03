4564f6b6e9994ccd8c62cc4c9fb3b3d521f25fe2
ngaionice <julian.myne@gmail.com>
2022-06-03 12:03:38 -0700

    chore: clean up unused code in Utils

--- app/src/main/java/me/ionice/snapshot/utils/Utils.kt
+++ app/src/main/java/me/ionice/snapshot/utils/Utils.kt
@@ -1,20 +1,8 @@
 package me.ionice.snapshot.utils
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import androidx.core.content.ContextCompat
 import java.time.format.DateTimeFormatter
 
 object Utils {
     val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy")
     val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm")
-
-    fun checkIfOnline(context: Context): Boolean {
-        val cm = ContextCompat.getSystemService(context, ConnectivityManager::class.java)
-
-        val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
-        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-    }
 }
