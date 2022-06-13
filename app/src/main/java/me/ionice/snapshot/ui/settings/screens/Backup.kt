3ef8a087ff5542d4afc653bebee71aeef0773f6b
ngaionice <julian.myne@gmail.com>
2022-06-12 18:20:13 -0700

    refactor: break down big function into smaller functions
    Break down the singular function into many such that each function is responsible for only 1 thing, and minimize non-deterministic code inside functions.
    
    Leaving the backup and restore functions unchanged to be fixed later.

--- app/src/main/java/me/ionice/snapshot/ui/settings/screens/Backup.kt
+++ app/src/main/java/me/ionice/snapshot/ui/settings/screens/Backup.kt
@@ -17,8 +17,17 @@ import kotlinx.coroutines.launch
 import me.ionice.snapshot.R
 import me.ionice.snapshot.ui.common.ConfirmationDialog
 import me.ionice.snapshot.ui.common.FunctionalityNotAvailableScreen
-import me.ionice.snapshot.ui.settings.*
+import me.ionice.snapshot.ui.settings.ProminentSwitchSetting
+import me.ionice.snapshot.ui.settings.SettingsRow
+import me.ionice.snapshot.ui.settings.SettingsUiState
+import me.ionice.snapshot.ui.settings.SignInButton
 import me.ionice.snapshot.utils.Utils
+import java.time.LocalDateTime
+
+// TODO:
+//  - move isBackupInProgress and the suspend functions to ViewModel;
+//    being able to cancel the backup if leaving the screen can be dangerous,
+//    and whether backup is in progress should not be decided by the UI
 
 @Composable
 fun BackupScreen(
@@ -34,94 +43,154 @@ fun BackupScreen(
     var isBackupInProgress by remember {
         mutableStateOf(false)
     }
-
-    var showBackupConfirmDialog by rememberSaveable { mutableStateOf(false) }
-    var showRestoreConfirmDialog by rememberSaveable { mutableStateOf(false) }
-
     if (!uiState.dataAvailable) {
-        FunctionalityNotAvailableScreen(reason = stringResource(R.string.settings_screen_backup_na_reason))
+        CannotBackupScreen()
     } else {
-        Column {
-            ProminentSwitchSetting(
-                mainLabel = stringResource(R.string.settings_screen_backup_main_switch),
-                checked = uiState.backupEnabled,
-                onCheckedChange = { onEnableBackup(!uiState.backupEnabled) }
-            )
-
-            AnimatedVisibility(
-                visible = uiState.backupEnabled,
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
-                Column {
-                    if (uiState.signedInGoogleAccountEmail != null) {
-                        // Show email or something
-                        SettingsRow(
-                            mainLabel = stringResource(R.string.settings_screen_backup_selected_account),
-                            secondaryLabel = uiState.signedInGoogleAccountEmail
-                        )
-                        SettingsRow(
-                            mainLabel = stringResource(R.string.settings_screen_backup_last_backup),
-                            secondaryLabel = uiState.lastBackupTime?.format(Utils.dateTimeFormatter)
-                                ?: stringResource(R.string.settings_screen_backup_last_backup_never)
-                        )
-
-                        Divider()
-
-                        if (!isBackupInProgress) {
-                            SettingsRow(
-                                mainLabel = stringResource(R.string.settings_screen_backup_start_backup),
-                                onClick = { showBackupConfirmDialog = true })
-                            SettingsRow(
-                                mainLabel = stringResource(R.string.settings_screen_backup_start_restore),
-                                onClick = { showRestoreConfirmDialog = true })
-                            ConfirmationDialog(
-                                isOpen = showBackupConfirmDialog,
-                                titleText = stringResource(R.string.settings_screen_backup_dialog_header),
-                                contentText = stringResource(R.string.settings_screen_backup_dialog_content),
-                                onConfirm = {
-                                    isBackupInProgress = true
-                                    toggleBottomNav(false)
-                                    scope.launch {
-                                        onStartBackup()
-                                        isBackupInProgress = false
-                                        toggleBottomNav(true)
-                                    }
-                                    showBackupConfirmDialog = false
-                                }, onCancel = {
-                                    showBackupConfirmDialog = false
-                                })
-                            ConfirmationDialog(
-                                isOpen = showRestoreConfirmDialog,
-                                titleText = stringResource(R.string.settings_screen_restore_dialog_header),
-                                contentText = stringResource(R.string.settings_screen_restore_dialog_content),
-                                onConfirm = {
-                                    isBackupInProgress = true
-                                    toggleBottomNav(false)
-                                    scope.launch {
-                                        onStartRestore()
-                                        isBackupInProgress = false
-                                        toggleBottomNav(true)
-                                    }
-                                    showRestoreConfirmDialog = false
-                                }, onCancel = {
-                                    showRestoreConfirmDialog = false
-                                })
-
-                        } else {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 24.dp), horizontalArrangement = Arrangement.Center
-                            ) {
-                                CircularProgressIndicator()
-                            }
-                        }
-                    } else {
-                        SignInButton(onSuccessfulLogin = onSuccessfulLogin)
-                    }
+        CanBackupScreen(
+            uiState = uiState,
+            isBackupInProgress = isBackupInProgress,
+            onEnableBackup = onEnableBackup,
+            onStartBackup = {
+                isBackupInProgress = true
+                toggleBottomNav(false)
+                scope.launch {
+                    onStartBackup()
+                    isBackupInProgress = false
+                    toggleBottomNav(true)
                 }
+            },
+            onStartRestore = {
+                isBackupInProgress = true
+                toggleBottomNav(false)
+                scope.launch {
+                    onStartRestore()
+                    isBackupInProgress = false
+                    toggleBottomNav(true)
+                }
+            },
+            onSuccessfulLogin = onSuccessfulLogin
+        )
+    }
+}
+
+@Composable
+private fun CannotBackupScreen() {
+    FunctionalityNotAvailableScreen(reason = stringResource(R.string.settings_screen_backup_na_reason))
+}
+
+@Composable
+private fun CanBackupScreen(
+    uiState: SettingsUiState.Backup,
+    isBackupInProgress: Boolean,
+    onEnableBackup: (Boolean) -> Unit,
+    onStartBackup: () -> Unit,
+    onStartRestore: () -> Unit,
+    onSuccessfulLogin: (GoogleSignInAccount) -> Unit
+) {
+    Column {
+        BackupEnabledToggle(isEnabled = uiState.backupEnabled, onIsEnabledChange = onEnableBackup)
+        AnimatedVisibility(
+            visible = uiState.backupEnabled, enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            if (uiState.signedInGoogleAccountEmail != null) {
+                BackupFunctionalities(
+                    isBackupInProgress = isBackupInProgress,
+                    email = uiState.signedInGoogleAccountEmail,
+                    lastBackupTime = uiState.lastBackupTime,
+                    onStartBackup = onStartBackup,
+                    onStartRestore = onStartRestore
+                )
+            } else {
+                SignInButton(onSuccessfulLogin = onSuccessfulLogin)
             }
         }
+    }
+}
+
+@Composable
+private fun BackupFunctionalities(
+    isBackupInProgress: Boolean,
+    email: String,
+    lastBackupTime: LocalDateTime?,
+    onStartBackup: () -> Unit,
+    onStartRestore: () -> Unit
+) {
+
+    Column {
+        BackupInfo(email = email, lastBackupTime = lastBackupTime)
+        Divider()
+        if (isBackupInProgress) {
+            BackupInProgress()
+        } else {
+            BackupActions(onStartBackup = onStartBackup, onStartRestore = onStartRestore)
+        }
+    }
+}
+
+@Composable
+private fun BackupEnabledToggle(isEnabled: Boolean, onIsEnabledChange: (Boolean) -> Unit) {
+    ProminentSwitchSetting(
+        mainLabel = stringResource(R.string.settings_screen_backup_main_switch),
+        checked = isEnabled,
+        onCheckedChange = { onIsEnabledChange(!isEnabled) }
+    )
+}
+
+@Composable
+private fun BackupInfo(email: String, lastBackupTime: LocalDateTime?) {
+    SettingsRow(
+        mainLabel = stringResource(R.string.settings_screen_backup_selected_account),
+        secondaryLabel = email
+    )
+    SettingsRow(
+        mainLabel = stringResource(R.string.settings_screen_backup_last_backup),
+        secondaryLabel = lastBackupTime?.format(Utils.dateTimeFormatter)
+            ?: stringResource(R.string.settings_screen_backup_last_backup_never)
+    )
+}
+
+@Composable
+private fun BackupActions(onStartBackup: () -> Unit, onStartRestore: () -> Unit) {
+    var showBackupDialog by rememberSaveable { mutableStateOf(false) }
+    var showRestoreDialog by rememberSaveable { mutableStateOf(false) }
+
+    SettingsRow(
+        mainLabel = stringResource(R.string.settings_screen_backup_start_backup),
+        onClick = { showBackupDialog = true })
+    SettingsRow(
+        mainLabel = stringResource(R.string.settings_screen_backup_start_restore),
+        onClick = { showRestoreDialog = true })
+    ConfirmationDialog(
+        isOpen = showBackupDialog,
+        titleText = stringResource(R.string.settings_screen_backup_dialog_header),
+        contentText = stringResource(R.string.settings_screen_backup_dialog_content),
+        onConfirm = {
+            onStartBackup()
+            showBackupDialog = false
+        }, onCancel = {
+            showBackupDialog = false
+        })
+    ConfirmationDialog(
+        isOpen = showRestoreDialog,
+        titleText = stringResource(R.string.settings_screen_restore_dialog_header),
+        contentText = stringResource(R.string.settings_screen_restore_dialog_content),
+        onConfirm = {
+            onStartRestore()
+            showRestoreDialog = false
+        }, onCancel = {
+            showRestoreDialog = false
+        })
+}
+
+@Composable
+private fun BackupInProgress() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 24.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
