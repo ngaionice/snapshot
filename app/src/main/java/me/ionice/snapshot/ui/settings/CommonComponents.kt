d506c03a57e7a5f44027959997a97f8025c600c4
ngaionice <julian.myne@gmail.com>
2022-06-18 21:25:20 -0700

    feat/refactor: add dropdown components, update param naming

--- app/src/main/java/me/ionice/snapshot/ui/settings/CommonComponents.kt
+++ app/src/main/java/me/ionice/snapshot/ui/settings/CommonComponents.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CloudSync
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -17,7 +18,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.common.api.ApiException
 import me.ionice.snapshot.R
 import me.ionice.snapshot.data.network.AuthResultContract
-import me.ionice.snapshot.ui.common.SectionHeader
+import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,13 +58,16 @@ fun SettingSwitch(
     checked: Boolean,
     onCheckedChange: (Boolean) -> Unit
 ) {
-    Row(modifier = Modifier.clickable(onClick = { onCheckedChange(!checked) }).padding(horizontal = 24.dp), verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = { onCheckedChange(!checked) })
+            .padding(horizontal = 24.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 16.dp)
-                .weight(1f)
-            , verticalAlignment = Alignment.CenterVertically
+                .weight(1f), verticalAlignment = Alignment.CenterVertically
         ) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = mainLabel, style = MaterialTheme.typography.titleLarge)
@@ -89,13 +93,13 @@ fun SettingsRow(
     mainLabel: String,
     secondaryLabel: String? = null,
     icon: ImageVector? = null,
-    loading: Boolean = false,
+    disabled: Boolean = false,
     onClick: (() -> Unit)? = null
 ) {
 
     // if a onClick is provided, enable button functionality
     val baseModifier =
-        onClick?.let { Modifier.clickable(enabled = !loading, onClick = onClick) } ?: Modifier
+        onClick?.let { Modifier.clickable(enabled = !disabled, onClick = onClick) } ?: Modifier
 
     Row(
         modifier = baseModifier
@@ -103,34 +107,28 @@ fun SettingsRow(
             .padding(horizontal = 24.dp, vertical = 16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        if (loading) {
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                CircularProgressIndicator()
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = mainLabel,
+                    modifier = Modifier.padding(end = 24.dp)
+                )
             }
-        } else {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                if (icon != null) {
-                    Icon(
-                        imageVector = icon,
-                        contentDescription = mainLabel,
-                        modifier = Modifier.padding(end = 24.dp)
-                    )
-                }
-                Column(modifier = Modifier.weight(1f)) {
-                    val textColor =
-                        if (onClick == null) MaterialTheme.colorScheme.onSurfaceVariant else MaterialTheme.colorScheme.onSurface
+            Column(modifier = Modifier.weight(1f)) {
+                val textColor =
+                    if (onClick == null || disabled) MaterialTheme.colorScheme.onSurfaceVariant else MaterialTheme.colorScheme.onSurface
+                Text(
+                    text = mainLabel,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = textColor
+                )
+                if (secondaryLabel != null) {
                     Text(
-                        text = mainLabel,
-                        style = MaterialTheme.typography.titleLarge,
-                        color = textColor
+                        text = secondaryLabel,
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
-                    if (secondaryLabel != null) {
-                        Text(
-                            text = secondaryLabel,
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onSurfaceVariant
-                        )
-                    }
                 }
             }
         }
@@ -161,8 +159,15 @@ fun SettingsGroup(title: String, content: @Composable () -> Unit) {
 
 @Composable
 private fun SettingsGroupHeader(title: String) {
-    Row(modifier = Modifier.padding(start = 24.dp, end = 16.dp, top = 8.dp, bottom = 8.dp), verticalAlignment = Alignment.CenterVertically) {
-        Text(text = title, style = MaterialTheme.typography.titleSmall, color = MaterialTheme.colorScheme.onPrimaryContainer)
+    Row(
+        modifier = Modifier.padding(start = 24.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+        )
     }
 }
 
@@ -208,10 +213,106 @@ fun SignInButton(onSuccessfulLogin: (GoogleSignInAccount) -> Unit) {
     SettingsRow(
         mainLabel = stringResource(R.string.settings_screen_backup_login),
         secondaryLabel = helperText,
-        loading = !enabled,
+        disabled = !enabled,
         onClick = {
             enabled = false
             authResultLauncher.launch(signInRequestCode)
             enabled = true
         })
+}
+
+@Composable
+fun TimePickerDialog(
+    title: String? = null,
+    isOpen: Boolean,
+    initialTime: LocalTime = LocalTime.MIDNIGHT,
+    onSelection: (LocalTime) -> Unit,
+    onClose: () -> Unit
+) {
+
+    var selectedHour by rememberSaveable { mutableStateOf(initialTime.hour) }
+    var selectedMinute by rememberSaveable { mutableStateOf((initialTime.minute / 5) * 5) }
+
+    val hours = (0..23).toList()
+    val minutes = (0..55 step 5).toList()
+
+    if (isOpen) {
+        AlertDialog(
+            onDismissRequest = onClose,
+            title = {
+                title?.let { Text(text = it) }
+            },
+            text = {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(Modifier.weight(1f)) {
+                        NumberDropdown(
+                            options = hours,
+                            selected = selectedHour,
+                            onSelection = { selectedHour = it })
+                    }
+                    Text(":")
+                    Box(Modifier.weight(1f)) {
+                        NumberDropdown(
+                            options = minutes,
+                            selected = selectedMinute,
+                            onSelection = { selectedMinute = it })
+                    }
+                }
+            },
+            confirmButton = {
+                Button(onClick = { onSelection(LocalTime.of(selectedHour, selectedMinute)) }) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = onClose) {
+                    Text(text = "Cancel")
+                }
+            })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun NumberDropdown(
+    label: String? = null,
+    options: List<Int>,
+    selected: Int,
+    onSelection: (Int) -> Unit
+) {
+    if (!options.contains(selected)) {
+        throw IllegalArgumentException("options does not contain initialValue")
+    }
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }) {
+        TextField(
+            readOnly = true,
+            value = selected.toString(),
+            onValueChange = {},
+            label = { label?.let { Text(it) } },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            options.forEach { selectionOption ->
+                DropdownMenuItem(
+                    text = { Text(selectionOption.toString()) },
+                    onClick = {
+                        onSelection(selectionOption)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
 }
