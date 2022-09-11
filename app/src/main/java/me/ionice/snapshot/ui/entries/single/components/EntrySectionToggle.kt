6beae7a9d164f2760646fcedb479ab46355a7bb6
ngaionice <julian.myne@gmail.com>
2022-09-11 14:52:21 -0400

    Add autofocus on edit, fix component consistencies

--- app/src/main/java/me/ionice/snapshot/ui/entries/single/components/EntrySectionToggle.kt
+++ app/src/main/java/me/ionice/snapshot/ui/entries/single/components/EntrySectionToggle.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import me.ionice.snapshot.ui.common.components.PageSectionContent
 import me.ionice.snapshot.ui.entries.single.EntrySection
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,26 +16,29 @@ import me.ionice.snapshot.ui.entries.single.EntrySection
 fun EntrySectionToggle(selectedProvider: () -> EntrySection, onSelect: (EntrySection) -> Unit) {
     val selected = selectedProvider()
 
-    Row(
-        modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        EntrySection.values().forEach {
-            if (it == selected) {
-                Button(onClick = {}) {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(imageVector = it.icon, contentDescription = it.description)
-                        Text(text = it.description)
+    PageSectionContent {
+        Row(
+            modifier = Modifier.padding(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            EntrySection.values().forEach {
+                if (it == selected) {
+                    Button(onClick = {}) {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(imageVector = it.icon, contentDescription = it.description)
+                            Text(text = it.description)
+                        }
                     }
-                }
-            } else {
-                FilledTonalIconButton(onClick = { onSelect(it) }) {
-                    Icon(imageVector = it.icon, contentDescription = it.description)
+                } else {
+                    FilledTonalIconButton(onClick = { onSelect(it) }) {
+                        Icon(imageVector = it.icon, contentDescription = it.description)
+                    }
                 }
             }
         }
     }
+
 }
--- app/src/main/java/me/ionice/snapshot/ui/entries/single/components/EntrySummarySection.kt
+++ app/src/main/java/me/ionice/snapshot/ui/entries/single/components/EntrySummarySection.kt
@@ -6,36 +6,66 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import me.ionice.snapshot.ui.common.components.PageSectionContent
 
 @Composable
 fun EntrySummarySection(editing: Boolean, text: String, onTextChange: (String) -> Unit) {
-    Box(modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)) {
-        if (editing) {
-            Column {
-                BasicTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = text,
-                    onValueChange = { if (it.length <= 140) onTextChange(it) },
-                    textStyle = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
-                    cursorBrush = SolidColor(LocalContentColor.current)
-                )
-                Row(
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    Text(
-                        text = (140 - text.length).toString(),
-                        style = MaterialTheme.typography.labelMedium
+    val focusRequester = remember { FocusRequester() }
+    val (textFieldValue, setTextFieldValue) = remember { mutableStateOf(TextFieldValue(text)) }
+
+    LaunchedEffect(key1 = text) {
+        setTextFieldValue(textFieldValue.copy(text = text))
+    }
+
+    LaunchedEffect(key1 = editing) {
+        if (editing) focusRequester.requestFocus()
+        setTextFieldValue(textFieldValue.copy(selection = TextRange(textFieldValue.text.length)))
+    }
+
+    PageSectionContent {
+        Box(modifier = Modifier.padding(horizontal = 24.dp)) {
+            if (editing) {
+                Column {
+                    BasicTextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
+                        value = textFieldValue,
+                        onValueChange = {
+                            // update this text manually first so it doesn't show text that is too long for a split second
+                            // but source of truth is still from text param
+                            val newText = if (it.text.length <= 140) it.text else text
+                            setTextFieldValue(it.copy(text = newText))
+                            onTextChange(newText)
+                        },
+                        textStyle = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+                        cursorBrush = SolidColor(LocalContentColor.current)
                     )
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Text(
+                            text = (140 - text.length).toString(),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
                 }
+            } else {
+                Text(text = text.ifBlank { "Start writing your summary!" })
             }
-        } else {
-            Text(text = text.ifBlank { "Start writing your summary!" })
         }
     }
 }
