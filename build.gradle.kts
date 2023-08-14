40a18fea66f75fc79e00c4faa4d64978cd68dc39
ngaionice <julian.myne@gmail.com>
2023-08-13 18:11:39 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     id("com.android.library") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
 
 tasks {
